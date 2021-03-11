5a39eb8706ff8320aeedbee5fba0df937233e596
davidmolina2810 <davidmolina2810@gmail.com>
2021-03-10 22:42:36 -0600

    removed links from logout yes and no btn, used history.push instead

--- seafood-frontend/src/containers/Logout.js
+++ seafood-frontend/src/containers/Logout.js
@@ -1,8 +1,8 @@
-import { Segment, Menu, Button } from 'semantic-ui-react'
-import { Link, Redirect } from 'react-router-dom'
+import { Segment, Button } from 'semantic-ui-react'
+import { useHistory } from 'react-router-dom'
 
 const Logout = ({ setUser }) => {
-
+  let history = useHistory()
   const logout = () => {
     localStorage.removeItem('auth_key')
     setUser(null)
@@ -15,18 +15,17 @@ const Logout = ({ setUser }) => {
       </p>
       <div>
         <Button.Group>
-          <Button onClick={logout}>
-            <Link className="logout-options" to="/home">
-                Yes
-            </Link>
+          <Button onClick={() => {
+            logout()
+            history.push('/home')
+          }}>
+            Yes
           </Button>
 
           <Button.Or />
           
-          <Button positive >
-            <Link className="logout-options" to="/profile">
-              No
-            </Link>
+          <Button positive onClick={() => history.push('/profile')}>
+            No
           </Button>
         </Button.Group>
       </div>
