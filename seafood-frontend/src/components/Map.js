aefa7105e13928f03038ef82e57a99cb645fc9d2
David Molina <davidmolina2810@gmail.com>
2021-05-23 22:35:37 -0500

    added google map to customer show page

--- seafood-frontend/src/components/Map.js
+++ seafood-frontend/src/components/Map.js
@@ -1,5 +1,6 @@
 import { React } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
+import { useState } from 'react';
 
 const mapStyles = {
   width: '50%',
@@ -7,8 +8,24 @@ const mapStyles = {
   margin: '0 auto'
 }
 
-const MapContainer = ({ props, company }) => {
-  console.log(company.latitude)
+const MapContainer = ({ company }) => {
+  const [ activeMarker, setActiveMarker ] = useState({});
+  const [ selectedPlace, setSelectedPlace ] = useState({});
+  const [ showingInfoWindow, setShowingInfoWindow ] = useState(false);
+
+  const onMarkerClick = (props, marker, e) => {
+    setActiveMarker(marker);
+    setSelectedPlace(props);
+    setShowingInfoWindow(true);
+  }
+
+  const onClose = props => {
+    if (showingInfoWindow) {
+      setShowingInfoWindow(false);
+      setActiveMarker(null);
+    }
+  }
+
   return(
     <Map
       google={window.google}
@@ -20,7 +37,22 @@ const MapContainer = ({ props, company }) => {
           lng: company.longitude
         }
       }
-    />
+    >
+      <Marker 
+        onClick={onMarkerClick}
+        name={company.name}
+      />
+
+      <InfoWindow
+        marker={activeMarker}
+        visible={showingInfoWindow}
+        onClose={onClose}
+      >
+        <div>
+          <h4>{selectedPlace.name}</h4>
+        </div>
+      </InfoWindow>
+    </Map>
   )
 }
 
