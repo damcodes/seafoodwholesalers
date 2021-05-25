69da2f76687614a27c6aba3aab07f555c92cb5a2
David Molina <davidmolina2810@gmail.com>
2021-05-25 00:05:37 -0500

    added search orders in admin view profile page

--- seafood-frontend/src/components/DailyOrders.js
+++ seafood-frontend/src/components/DailyOrders.js
@@ -2,55 +2,49 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import { Icon, List, Input, Button } from 'semantic-ui-react'
+import Adapter from '../adapters/Adapter'
 
 const DailyOrders = () => {
 
-  const [ allOrders, setAllOrders ] = useState([])
-  const [ date, setDate ] = useState(null)
-  const [ filtered, setFiltered ] = useState(false)
+  const [ allOrders, setAllOrders ] = useState([]);
+  const [ date, setDate ] = useState(null);
+  const [ filtered, setFiltered ] = useState(false);
+  const [ orderNumber, setOrderNumber ] = useState(null);
 
   useEffect(() => {
-    fetch(`http://localhost:3001/orders`, {
-      method: "GET",
-      headers: {
-        "Content-type":"application/json",
-        "Authorization": localStorage.getItem('auth_key')
-      }
-    })
+    Adapter.fetch("GET", "orders")
     .then( res => res.json() )
     .then( data => setAllOrders(data) )
   }, [])
-  
-  useEffect(() => {
-    console.log(date)
-  }, [ date ])
 
   const filteredOrders = orders => {
-    // debugger
-    const filtered = orders.filter( order => order.created_at.slice(0,10) === date)
+    const filtered = orderNumber ? orders.filter( order => order.created_at.slice(0,10) === date && order.order_number.toString().includes(orderNumber)) : orders.filter( order => order.created_at.slice(0,10) === date)
+    console.log(filtered)
     return(
-      <List className='order-card-list' selection verticalAlign="middle">
-        { filtered.length > 0 ? 
-        filtered.map( order => {
-          return(
-            <List.Item key={order.id} as='a'>
-              {/* <Icon name='angle double right' /> */}
-              <Link to={`/orders/${order.id}`}>
-                <List.Content >
-                  <List.Header >#{order.order_number}</List.Header>
-                </List.Content>
-              </Link>
-            </List.Item>
-          )
-        })
-        :
-        <List.Item>
-          <List.Content>
-            <List.Header>No Orders</List.Header>
-          </List.Content>
-        </List.Item>
-        }
-      </List>
+      <>
+        <Input onChange={e => setOrderNumber(e.target.value)} id="search-orders" type="text" size="small" placeholder="Search Order Number..."/>
+        <List className='order-card-list' selection verticalAlign="middle">
+          { filtered.length > 0 ? 
+          filtered.map( order => {
+            return(
+              <List.Item key={order.id} as='a'>
+                <Link to={`/orders/${order.order_number}`}>
+                  <List.Content >
+                    <List.Header >#{order.order_number}</List.Header>
+                  </List.Content>
+                </Link>
+              </List.Item>
+            )
+          })
+          :
+          <List.Item>
+            <List.Content>
+              <List.Header>No Orders</List.Header>
+            </List.Content>
+          </List.Item>
+          }
+        </List>
+      </>
     )
   }
 
@@ -58,7 +52,6 @@ const DailyOrders = () => {
     <div>
       <Input type="date" onChange={e => {
           console.log(e.target.value)
-          // console.log(Date(e.target.value).getDay())
             setDate(e.target.value.slice(0,10))
       }}/>
       <Button id="orders-by-date" floated='right' onClick={() => setFiltered(!filtered)} circular='true' >
